a33d4ece3c5621324afdc8f01c4ba29a66aa5157
george-cm <george.murga+github@gmail.com>
2021-04-29 18:15:35 +0300

    Add some details.

--- setup.py
+++ setup.py
@@ -4,4 +4,14 @@ setup(
     name='aem_naming_utils',
     version='0.1',
     packages=find_packages(),
+    author="George Murga",
+    author_email="george.murga+aemnamingutils@gmail.com",
+    description="Package to work with AEM file names and urls.",
+    url="https://github.com/george-cm/aem-naming-utils.git",
+    python_requires='>=3.7',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GPL3 License",
+        "Operating System :: OS Independent",
+    ],
 )
